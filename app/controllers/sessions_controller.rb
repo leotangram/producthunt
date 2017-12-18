32b2eaa22289b7e243ebd1b613665ee0db8044af
leotangram <leotangram@gmail.com>
2017-12-18 16:40:46 -0500

    Implementa la funcionalidad de salir bien

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,16 @@ class SessionsController < ApplicationController
 
   def create
   	 user = User.find_by(email: params[:email])
-  if user && user.authenticate(params[:password])
-    sign_in(user)
-    redirect_to root_path
-  else
-    render :new
+    if user && user.authenticate(params[:password])
+      sign_in(user)
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  def destroy
+    sign_out
+    redirect_to root_path
   end
 end
