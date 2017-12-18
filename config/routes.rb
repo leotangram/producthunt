3f5823fa5a0cfee4ba4aa1fe49e353fc22b463d9
leotangram <leotangram@gmail.com>
2017-12-18 16:32:03 -0500

    Implementa la funcionalidad de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'sessions/new'
+  root 'products#index'
 
-=======
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
   resources :users, only: [:new, :create]
->>>>>>> ea736d5d442343e85e4f97409c8d669b920bd94a
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # get	'/products', to: 'products#index'
+  # get '/products', to: 'products#index'
   # get '/products/new', to: 'products#new', as: 'new_product'
   # post '/products', to: 'products#create'
   # get '/products/:id', to: 'products#show', as: 'product'
@@ -18,5 +19,4 @@ Rails.application.routes.draw do
   resources :products
   # resources :products, only: [:index]
   # resources :products, except: [:index, :create, :index]
-  root 'products#index'
 end
